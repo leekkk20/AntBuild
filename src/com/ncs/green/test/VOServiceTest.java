20c8aeb8dd6d013aad6bc699423487cd9e26b6b6
DESKTOP-B2PK687\leekk <leekkk20@naver.com>
2022-10-17 12:55:28 +0900

    Test01_commit Test

---
+++ src/com/ncs/green/test/VOServiceTest.java
@@ -0,0 +1,29 @@
+package com.ncs.green.test;
+
+import com.ncs.green.service.VOService;
+import com.ncs.green.vo.MemberVO;
+
+public class VOServiceTest {
+
+	public static void main(String[] args) {
+
+		// 1) MemberVO 생성
+		MemberVO vo = new MemberVO();
+		vo.setId("banana");
+		vo.setPassword("12345!");
+		vo.setName("홍길동");
+		vo.setAge(100);
+
+		// 2) 직접 사용 출력
+		System.out.println("** 직접 출력 => " + vo);
+
+		// 3) VOService를 이용한 출력
+		// => VOService 생성, 초기화
+		VOService service = new VOService(); // VOService 생성
+		service.setMemberVO(vo); // VOService 초기화
+		System.out.println("** AntBuild Test **");
+		System.out.println("** VOService 출력 => " + service.getMemberVO());
+
+	} // main
+	
+} // class
